9359679cb06162cd0585dce4a81723f5bfc42bb7
dnea07 <nathania07@gmail.com>
2023-03-31 15:40:28 +0700

    update hilangin console.log

--- projects/client/src/reducers/userReducer.js
+++ projects/client/src/reducers/userReducer.js
@@ -5,7 +5,6 @@ const INITIAL_STATE = {
 };
 
 export const userReducer = (state = INITIAL_STATE, action) => {
-  console.log("data dr fungsi user", action);
   switch (action.type) {
     case "LOGIN_SUCCESS":
       return { ...state, ...action.payload };
