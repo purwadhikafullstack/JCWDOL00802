536f3faebf10a00d58db888f99a7a13ee522687f
SilenceIsNotFun <silenceisnotfun@gmail.com>
2023-04-06 08:17:21 +0700

    [eck-28] orderlist

--- projects/server/src/routers/index.js
+++ projects/server/src/routers/index.js
@@ -1,7 +1,12 @@
+const express = require("express");
 const userRouter = require("./user");
 const productRouter = require("./product");
+const TransactionRouter = require("./transaction");
 
-module.exports = {
-  userRouter,
-  productRouter,
-};
+const router = express.Router();
+
+router.use("/user", userRouter);
+router.use("/product", productRouter);
+router.use("/trans", TransactionRouter);
+
+module.exports = router;
